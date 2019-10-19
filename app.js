21a3b356919e0f8b25e836fb1b5c3c799827845d
Denys Potapov <denys.potapov@gmail.com>
2019-10-20 02:42:58 +0300

    replace with code mirror

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
-const flask = new CodeFlask('#editor', {
-  lineNumbers: false
+CodeMirror.defineMode("fad", function() {
+
+  return {
+  	startState: function(){
+	  	return {
+	  		inVar: false,
+	  	};
+	},
+    token: function(stream, state) {
+      if (stream.sol()) {
+      	if (stream.string.substring(0, 3) != '   ') {
+      		stream.skipToEnd();
+      		return 'comment';
+      	}
+      }
+      stream.eatSpace();
+      
+      l = stream.next();
+      // digits or index
+      if (/\d/.test(l) && state.inVar) {
+      	stream.match(/\d/);
+      	state.inVar = false;
+      	return 'number'
+      };
+
+      if (stream.eol() || (l == ',') || (l.toLowerCase() == l)) {
+      	state.inVar = false;
+      	return
+      }
+      state.inVar = true;
+      stream.match(/^\s\d\,/)
+      return 'variable';
+    }
+  };
+});
+
+var editor = new CodeMirror.fromTextArea(
+	document.getElementById('source'), {
+    lineNumbers: true,
+    lineWrapping: true,
+    mode: "fad",
+    theme: "fad",
+    indentUnit: 3,
+    tabSize: 3
+ });
+
+editor.setOption("extraKeys", {
+  Tab: function(cm) {
+    var spaces = Array(cm.getOption("indentUnit") + 1).join(" ");
+    cm.replaceSelection(spaces);
+  }
 });
